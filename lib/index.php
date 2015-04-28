7b2b220bcd97c3cdd025fdc33ed8333e6be5ed30
Juhi <juhi.saxena@rtcamp.com>
2015-04-28 12:25:03 +0530

    Updated conditions

--- lib/index.php
+++ lib/index.php
@@ -3,6 +3,6 @@
  * It was a place without a single feature of the space-time matrix that he
  * knew. It was a place where nothing yet had happened - an utter emptiness.
  * There was neither light nor dark: there was nothing here but emptiness.
- * Clifford D. Simak
+ * Clifford D. Simak vv
  */
 ?>
